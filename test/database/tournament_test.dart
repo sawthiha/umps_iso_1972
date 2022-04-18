d6fda935fce15bc80f32b4a8039b6cc1694b870e
sawthiha <sawthiha@mtu.edu.mm>
2022-04-18 16:35:40 +0900

    Tournament DB test layout!

---
+++ test/database/tournament_test.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main()  {
+  setUp(
+    ()  {
+
+    }
+  );
+
+  group('CRUD Tests',
+    ()  {
+      test('Create',
+        ()  {
+          
+        }
+      );
+      test('Read',
+        ()  {
+
+        }
+      );
+      test('Update',
+        ()  {
+
+        }
+      );
+      test('Delete',
+        ()  {
+
+        }
+      );
+    }
+  );
+
+  tearDown(
+    ()  {
+
+    }
+  );
+}
