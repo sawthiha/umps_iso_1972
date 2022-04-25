bbbbb9789cd5865bd2212c46777c9a86ba960833
sawthiha <sawthiha@mtu.edu.mm>
2022-04-25 22:27:10 +0900

    AppBinding initiated with Database!

--- lib/bindings.dart
+++ lib/bindings.dart
@@ -5,6 +5,17 @@ class AppBindings extends Bindings  {
   @override
   void dependencies() {
     final store = openStore();
+    Get.put<Database>(
+      Database(
+        store: store,
+        competitorBox: store.box<Competitor>(),
+        judgeBox: store.box<Judge>(),
+        scoreBox: store.box<Score>(),
+        scoreboardEntryBox: store.box<ScoreboardEntry>(),
+        stageBox: store.box<Stage>(),
+        tournamentBox: store.box<Tournament>(),
+      ),
+    );
   }
 
 }
