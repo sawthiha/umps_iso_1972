fb6e94282fe3e4a618134b5e18b61d957747029c
sawthiha <sawthiha@mtu.edu.mm>
2022-05-06 23:51:26 +0900

    Tournament create() method implemented!

--- lib/database/tournament.dart
+++ lib/database/tournament.dart
@@ -1,8 +1,22 @@
 part of database;
 
-mixin TournamentDB  {
+mixin TournamentDBMixin  {
 
   Box<Tournament> get tournamentBox;
+  
+  Tournament createTournament({
+    required String name,
+    String? description,
+    required Iterable<Judge> judges,
+    required Iterable<Competitor> competitors,
+  })  {
+    final tournament = Tournament(name: name)
+      ..description = description
+      ..competitors.addAll(competitors)
+      ..judges.addAll(judges);
+    tournamentBox.put(tournament);
+    return tournament;
+  }
 
 }
 
